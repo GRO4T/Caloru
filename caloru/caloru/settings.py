4aa7e42d24786d9e75aa01f36c2804fc325a8b38
Damian Kolaska <damian.kolaska99@gmail.com>
2022-12-27 00:30:35 +0100

    Deploy Django welcome page using Apache WSGI

--- caloru/caloru/settings.py
+++ caloru/caloru/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +25,7 @@ SECRET_KEY = "django-insecure-l@3zjdtnq_))c(#7p#2q88axo^lq-6e0rudfsm7x!f_urclm62
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: List[str] = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
