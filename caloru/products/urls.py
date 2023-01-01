786ed4bdd6e13f360243c526176bdff876af1e74
Damian Kolaska <damian.kolaska99@gmail.com>
2023-01-01 14:04:33 +0100

    Bootstrap dashboard

--- caloru/products/urls.py
+++ caloru/products/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.products, name="products"),
+    path("edit/", views.edit_product_form, name="edit_product"),
+    path("add/", views.add_product_form, name="add_product"),
 ]
--- caloru/products/views.py
+++ caloru/products/views.py
@@ -12,3 +12,20 @@ def products(request):
     template = loader.get_template("products.html")
     context = {"product_list": product_list}
     return HttpResponse(template.render(context, request))
+
+
+def add_product_form(request):
+    template = loader.get_template("details.html")
+    context = {"product_action": "add"}
+    return HttpResponse(template.render(context, request))
+
+
+def add_product(request):
+    print("Adding product...")
+    return HttpResponse("Product added")
+
+
+def edit_product_form(request):
+    # template = loader.get_template("details.html")
+    # return HttpResponse(template.render({}, request))
+    return HttpResponse("HELLO")
