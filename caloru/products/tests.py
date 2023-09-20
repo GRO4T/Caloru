be5800323c247a8b23e724f895c5ac0d56df19fe
Damian Kolaska <damian.kolaska99@gmail.com>
2023-09-20 18:34:03 +0000

    Add simple unit test

--- caloru/products/tests.py
+++ caloru/products/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from products.models import Product
+
+
+class LocalDatabaseTestCase(TestCase):
+    def test_local_database_is_working(self):
+        Product.objects.create(
+            name="Milk", weight=2000, calories=200, protein=30, carbs=10, fat=10
+        )
+        self.assertTrue(Product.objects.filter(name="Milk").exists())
+        Product.objects.filter(name="Milk").delete()
+        self.assertFalse(Product.objects.filter(name="Milk").exists())
