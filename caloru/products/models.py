a0a2c4d39eaeaea1a67f7170594150edeca087c5
Damian Kolaska <damian.kolaska99@gmail.com>
2023-09-21 18:23:20 +0000

    Update models

--- caloru/products/models.py
+++ caloru/products/models.py
@@ -1,23 +1,58 @@
 from django.db import models
 from django.conf import settings
 
-# Create your models here.
+
+class User(models.Model):
+    """User profile."""
+
+    username = models.CharField(max_length=255)
 
 
 class Product(models.Model):
+    """Product from the catalog.
+
+    NOTE: All nutrient contents are expressed per 100g.
+    """
+
     name = models.CharField(max_length=255)
-    calories = models.FloatField()
+    name_2 = models.CharField(max_length=255)
+
+    water = models.FloatField()
+    energy = models.FloatField()
     protein = models.FloatField()
     carbs = models.FloatField()
     fat = models.FloatField()
 
+    vitamin_a = models.FloatField()
+    vitamin_b1 = models.FloatField()
+    vitamin_b2 = models.FloatField()
+    vitamin_b3 = models.FloatField()
+    vitamin_b5 = models.FloatField()
+    vitamin_b6 = models.FloatField()
+    vitamin_b12 = models.FloatField()
+    vitamin_c = models.FloatField()
+    vitamin_d = models.FloatField()
+    vitamin_e = models.FloatField()
+    vitamin_k = models.FloatField()
 
-class User(models.Model):
-    username = models.CharField(max_length=255)
+    calcium = models.FloatField()
+    copper = models.FloatField()
+    iron = models.FloatField()
+    magnesium = models.FloatField()
+    manganese = models.FloatField()
+    phosphorus = models.FloatField()
+    potassium = models.FloatField()
+    selenium = models.FloatField()
+    sodium = models.FloatField()
+    zinc = models.FloatField()
+
+    cholesterol = models.FloatField()
 
 
 class ConsumedProduct(models.Model):
+    """Consumed product added to tracker."""
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     date = models.DateField()
-    quantity = models.IntegerField()
+    amount_in_grams = models.IntegerField()
--- caloru/products/tests.py
+++ caloru/products/tests.py
@@ -5,7 +5,37 @@ from products.models import Product
 
 class LocalDatabaseTestCase(TestCase):
     def test_local_database_is_working(self):
-        Product.objects.create(name="Milk", calories=200, protein=30, carbs=10, fat=10)
+        Product.objects.create(
+            name="Milk",
+            name_2="Whole (3.25% fat)",
+            water=88.32,
+            energy=59.84,
+            protein=3.22,
+            carbs=4.52,
+            fat=3.25,
+            vitamin_a=27.87,
+            vitamin_b1=0.044,
+            vitamin_b2=0.183,
+            vitamin_b3=0.107,
+            vitamin_b5=0,
+            vitamin_b6=0,
+            vitamin_b12=0,
+            vitamin_c=0,
+            vitamin_d=0,
+            vitamin_e=0,
+            vitamin_k=0,
+            calcium=0,
+            copper=0,
+            iron=0,
+            magnesium=0,
+            manganese=0,
+            phosphorus=0,
+            potassium=0,
+            selenium=0,
+            sodium=0,
+            zinc=0,
+            cholesterol=0,
+        )
         self.assertTrue(Product.objects.filter(name="Milk").exists())
         Product.objects.filter(name="Milk").delete()
         self.assertFalse(Product.objects.filter(name="Milk").exists())
