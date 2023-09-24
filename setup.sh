48152c7a3b3973db259334b40ecdde3ffb155864
Damian Kolaska <damian.kolaska99@gmail.com>
2023-09-24 15:03:53 +0000

    fix

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-cd ./caloru || exit
+(
+    cd ./caloru || exit
 
-python manage.py migrate
+    python manage.py migrate
 
-python manage.py loaddata users
-python manage.py loaddata products
-python manage.py loaddata consumed_products
+    python manage.py loaddata users
+    python manage.py loaddata products
+    python manage.py loaddata consumed_products
 
-python manage.py ensure_superuser --no-input \
-    --username "$DJANGO_SUPERUSER_USERNAME" \
-    --email "$DJANGO_SUPERUSER_EMAIL" \
-    --password "$DJANGO_SUPERUSER_PASSWORD"
+    python manage.py ensure_superuser --no-input \
+        --username "$DJANGO_SUPERUSER_USERNAME" \
+        --email "$DJANGO_SUPERUSER_EMAIL" \
+        --password "$DJANGO_SUPERUSER_PASSWORD"
+)
