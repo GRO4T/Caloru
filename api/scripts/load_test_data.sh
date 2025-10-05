d5b9e959b25338322b0639f8e4424161b3cf8481
Damian Kolaska <damian.kolaska99@gmail.com>
2025-10-05 21:14:53 +0200

    fix: loading users and ensure_superuser command

--- api/scripts/load_test_data.sh
+++ api/scripts/load_test_data.sh
@@ -3,7 +3,5 @@
     cd ./caloru || exit
 
     python manage.py loaddata users
-    python manage.py loaddata products
-    python manage.py loaddata consumed_products
     python manage.py loaddata food_items
 )
